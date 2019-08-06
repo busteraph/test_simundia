7a3acad75ae94fbd7b9a22929254b082f1af614b
Raphook <raphael69libert@gmail.com>
2019-08-06 18:07:04 +0200

    getting collaborator fullname from email

--- email_parser.rb
+++ email_parser.rb
@@ -7,7 +7,13 @@ input_csv = ARGV[0]
 
 # loop through each record in the csv file
 CSV.foreach(input_csv) do |row1|
-	puts row1[0]
+	#check email validity thru regexp
+	valid_email = row1[0].scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
+	
+	valid_email.each do |ve| 
+		full_name = ve.split('@')[0]
+	 	puts full_name
+	end 	
 end
 
 
