dff3c7e696cb7438e94a4b7aa85ac67cecdaabf8
Raphook <raphael69libert@gmail.com>
2019-08-06 17:48:41 +0200

    toh

--- email_parser.rb
+++ email_parser.rb
@@ -5,8 +5,7 @@ require 'csv'
 # get the input filename from the command line
 input_csv = ARGV[0]
 
-# loop through each record in the csv file, adding
-# each record to our array.
+# loop through each record in the csv file
 CSV.foreach(input_csv) do |row1|
 	puts row1.inspect
 end
