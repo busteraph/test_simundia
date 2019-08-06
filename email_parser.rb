17b91e598c54a7f30c9addc6b2e8a017c5dffa7d
Raphook <raphael69libert@gmail.com>
2019-08-06 17:47:45 +0200

    output each row of csv file

---
+++ email_parser.rb
@@ -0,0 +1,14 @@
+require 'csv'
+# program: email_parser.rb
+# usage:   ruby email_parser.rb input_filename > output_filename
+
+# get the input filename from the command line
+input_csv = ARGV[0]
+
+# loop through each record in the csv file, adding
+# each record to our array.
+CSV.foreach(input_csv) do |row1|
+	puts row1.inspect
+end
+
+
