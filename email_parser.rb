f3593562d7e60b2800bed93e8b89ffe99794ba66
Raphook <raphael69libert@gmail.com>
2019-08-07 16:11:09 +0200

    write csv output file from list of collaborator

--- email_parser.rb
+++ email_parser.rb
@@ -2,9 +2,11 @@ require 'csv'
 # program: email_parser.rb
 # usage:   ruby email_parser.rb input_filename > output_filename
 
+SCOPE_ID = 16
+
 # checks number of command line arguments
 unless ARGV.length == 1
-  puts "Exécution impossible sans argments"
+  puts "Exécution impossible sans arguments"
   puts "usage : ruby email_parser.rb input_filename > output_filename\n"
   exit
 end
@@ -12,17 +14,29 @@ end
 # get the input filename from the command line
 input_csv = ARGV[0]
 
+# store Collaborator entries
+collab_array = [['first_name', 'last_name', 'email', 'scope_id']]
+
 # loop through each record in the csv file
 CSV.foreach(input_csv) do |row1|
 	#check email validity thru regexp
 	valid_email = row1[0].scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
 	
 	valid_email.each do |ve| 
+		email = ve.to_s
 		full_name = ve.split('@')[0]
-		first_name = full_name.split('.')[0]
-		last_name = full_name.split('.')[1]
-	 	puts " #{first_name} #{last_name} "
+		first_name = full_name.split('.')[0].capitalize
+		last_name = full_name.split('.')[1].capitalize
+		scope_id = SCOPE_ID
+	 	# puts " #{first_name} #{last_name}  #{email}  #{scope_id}"
+		collab_array.push([first_name, last_name, email, scope_id])  
 	end 	
 end
 
+# write collaborator list to csv file
+CSV.open('collab_list.csv', 'w') do |csv|
+  collab_array.each do |row|
+	  csv << row
+	end 
+end
 
