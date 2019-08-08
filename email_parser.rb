cc5bbe8957c26dc9ac3c8ce2b2ede539d0f68491
Raphook <raphael69libert@gmail.com>
2019-08-08 22:27:11 +0200

    final commit : stronger tests and few minor changes

--- email_parser.rb
+++ email_parser.rb
@@ -4,35 +4,38 @@
 require 'csv'
 require './collaborator'
 
-# 
+COMPANY_A = 'A_Company'
 SCOPE_ID = 16
 
 # get the input filename from the command line
 input_csv = ARGV[0]
 
+output_csv = "#{File.expand_path(File.dirname(__FILE__))}/collab_list_#{COMPANY_A}.csv"
+
 def output_collab_list(input_csv)
 	collab_array = [['first_name', 'last_name', 'email', 'scope_id']]
-	# get each record in the csv file
+	
 	CSV.foreach(input_csv) do |row1|
 		#check email validity thru regexp
 		valid_email = row1[0].scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)
 		
+		# push collaborators in an array 
 		valid_email.each do |ve| 
 			email = ve.to_s
 			full_name = ve.split('@')[0]
 			first_name = full_name.split('.')[0].capitalize
 			last_name = full_name.split('.')[1].capitalize
 			scope_id = SCOPE_ID
-			collab = Collaborator.new(first_name, last_name, email, scope_id)
+			collab = Collaborator.new(first_name, last_name, scope_id, email)
 			collab_array.push(collab.to_csv)  
 		end 	
 	end
 	collab_array
 end
 
-def write_csv_output(ca)
+def write_output_csv(output_csv, ca)
 	# write collaborator list to csv file
-	CSV.open('collab_list.csv', 'w') do |csv|
+	CSV.open(output_csv, 'w') do |csv|
 	  ca.each do |row|
 		  csv << row
 		end 
@@ -48,6 +51,6 @@ if $0 == __FILE__
 	end
 
   # execute the scrpit
-  write_csv_output(output_collab_list(input_csv))
-  puts "Collaborator file completed"
+  write_output_csv(output_csv, output_collab_list(input_csv))
+  puts "Collaborators file completed"
 end
--- test/TC_email_parser.rb
+++ test/TC_email_parser.rb
@@ -1,22 +1,32 @@
 # Unit tests for email_parser.rb
 
 require "test/unit"
-require File.expand_path(File.dirname(__FILE__)) + "/../email_parser.rb"
+require "#{File.expand_path(File.dirname(__FILE__))}/../email_parser.rb"
 
 class TC_EmailParser < Test::Unit::TestCase
-      
-  # test that the method return an array
+
   def test_output_collab_list
-    input_file = 'collab_list.csv'
-    assert_kind_of(Array, output_collab_list(input_file))  
+    expected_result = [["first_name", "last_name", "email", "scope_id"], 
+                      ["Alain", "Dieckhoff", 16, "alain.dieckhoff@entreprisea.fr"], 
+                      ["Alexandre", "Desrumaux", 16, "alexandre.desrumaux@entreprisea.fr"], 
+                      ["Alice", "Antheaume", 16, "alice.antheaume@entreprisea.fr"]]
+
+    assert_equal(expected_result, output_collab_list(input_csv_file))
   end
 
-  # test that the writing of oput csv file
-  def test_write_csv_output
-    input_file = 'collab_list.csv'
-    output_array = output_collab_list(input_file)
-    write_csv_output(output_array)
-    assert_path_exist(File.expand_path(File.dirname(__FILE__)) + "/../collab_list.csv")  
+  def test_write_output_csv
+    collab_array = output_collab_list(input_csv_file)
+    assert_kind_of(Array, collab_array)  
+    write_output_csv(output_csv_file, collab_array)
+    assert_path_exist("#{File.expand_path(File.dirname(__FILE__))}/support/collab_list.csv")  
   end
 
+  def input_csv_file
+    "#{File.expand_path(File.dirname(__FILE__))}/support/company_emails.csv"
+  end
+      
+  def output_csv_file
+    "#{File.expand_path(File.dirname(__FILE__))}/support/collab_list.csv"
+  end
+      
 end
